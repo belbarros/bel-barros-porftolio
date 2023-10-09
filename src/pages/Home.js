1c4bd39dab7796dc9c5ff362e8e117690427cefb
Izabel de Barros <izabelfeigel@gmail.com>
2023-10-09 13:50:39 -0300

    FEAT: Router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div>
         <h1>Olá,</h1>
         <h1>sou a Bel!</h1>
-        <h3>Por favor me de um emprego.</h3>
+        <h3>Textinho.</h3>
         <h6>Tags</h6>
       </div>
     </div>

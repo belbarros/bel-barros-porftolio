6d5bf620015493e97e2afcfd6e76e67eb1c18080
Izabel de Barros <izabelfeigel@gmail.com>
2023-10-09 13:49:25 -0300

    FEAT: Router installed

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+        <Header/>
+      <div>
+        <h1>Olá,</h1>
+        <h1>sou a Bel!</h1>
+        <h3>Por favor me de um emprego.</h3>
+        <h6>Tags</h6>
+      </div>
+    </div>
+  );
+}
